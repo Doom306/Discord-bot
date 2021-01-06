ff43e457f027d2a9130cf467bf17f8cb6fc942ea
Doom <76986040+Doom306@users.noreply.github.com>
2021-01-06 09:45:32 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        String token = "NzYxMDcyNjMyOTU4NjgxMDk5.X3VSHw.uQHU2ZNRi52wYC6IqdbP5o2JkTo";
+        String token = "TOKEN";
         Bot bot = new Bot(token);
         bot.build();
     }
